a9c1e5230ec806ee8d85db1bf24d2f1780ba2bbd
empty <studio@empty.pro>
2015-06-30 23:16:06 +0300

    edit converter

--- getrates.php
+++ getrates.php
@@ -3,9 +3,6 @@
  * Этот скрипт получает возвращает курсы валют в xml формате.
  * Источник данных - сервер pfsoft.com.ua
  * После получения данные сохраняются в кеше.
- * 
- * @author Стаценко Владимир
- * @link http://www.simplecoding.org
  */
 
 require_once('ratescache.php');
@@ -15,8 +12,9 @@ $rCache = new RatesCache();
 
 //пробуем получить данные из кеша
 if (FALSE === ($data = $rCache->get())) {
-	//если данные в кеше устарели, пытаемся получить их от сервера pfsoft.com.ua
-	$data = @file_get_contents('http://pfsoft.com.ua/service/currency/');
+	//если данные в кеше устарели, пытаемся получить их от сервера
+    $today = date("d/m/Y");
+	$data = @file_get_contents('http://www.cbr.ru/scripts/XML_daily.asp?date_req='.$today);
 	//если сервер недоступен, пробуем получить данные из устаревшего кеша
 	if (FALSE === $data) {
 		$data = $rCache->get(TRUE);
--- js/script.js
+++ js/script.js
@@ -1,24 +1,57 @@
-/**
- * @author Statsenko Vladimir
- * @link http://www.simplecoding.org
-*/
-var curCodes = ['RUB', 'USD', 'EUR'];
+var curCodes = ['USD'];
+var curRate = 0;
+var rateTax = 1.05;
 $(document).ready(function() {
 	$.get('getrates.php', function(data) {
 		if (data != 'ERR') {
 			var rates = '';
+            $(data).each(function(){
+                if($(this).attr('Date')!=undefined) {
+                    var rate_data = $(this).attr('Date');
+                    var temp = $(this).attr('Date');
+                    var splArr = temp.split('/');
+                    splArr[2] = splArr[2].substring(2,4);
+                    rate_data = splArr.join('.');
+                    $('#rate-data').html(rate_data);
+                }
+            });
+
 			$(data).find('Valute').each(function(key, value) {
 				var curCode = $(value).find('CharCode').html();
 				if (-1 != $.inArray(curCode, curCodes)) {
 					rates += '<li>' + $(value).find('Nominal').html()
 						+ ' ' + curCode
-						+ ' = ' + $(value).find('Value').html() + ' грн.' + '</li>';
+						+ ' = ' + $(value).find('Value').html() + ' руб.' + '</li>';
+                    curRate = $(value).find('Value').html().substring(0,5);
+                    curRate = curRate.replace(/,/g, "\.");
+                    curRate = parseFloat(curRate)*rateTax;
+                    curRate = curRate.toString().substring(0,5);
 				}
 				$('#rates').html(rates);
+                $('#rate-cur').html(curRate);
+                $('#rate-rur').val(curRate);
 			});
 		}
 		else {
 			$('#rates').html('<li>Данные недоступны</li>');
+            $('#rate-cur').html('ERR');
 		}
 	});
+
+    setInterval(function(){
+        var curVal = $("#rate-usd").val();
+        var prevVal  = $("#rate-usd").data("prevVal") || null;
+
+        if (prevVal !== curVal) {
+            var rateUsd = $("#rate-usd").val().replace(/,/g, "\.");
+            curRate = curRate.replace(/,/g, "\.");
+            var rateRur = (parseFloat(rateUsd)*parseFloat(curRate)).toFixed(2);
+            if(!isNaN(rateRur)) {
+                $("#rate-rur").val(rateRur);
+            } else {
+                $("#rate-rur").val("-");
+            }
+        }
+    }, 100);
+
 });
