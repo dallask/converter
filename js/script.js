cfcd62cc9f8e49367448a17228dd7214421013be
empty <studio@empty.pro>
2015-07-03 14:01:55 +0300

    replace XML feed to JSON feed

--- js/script.js
+++ js/script.js
@@ -24,35 +24,30 @@ $(document).ready(function() {
 
     var curDate = (curr_day < 10 ? '0' : '') + curr_day + "%2F" + (curr_month < 10 ? '0' : '') + curr_month + "%2F" + curr_year;
 
-	$.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D%27http%3A%2F%2Fwww.cbr.ru%2Fscripts%2FXML_daily.asp%3Fdate_req%3D' + curDate + '%27&format=xml&callback=', function(data) {
+	$.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D%27http%3A%2F%2Fwww.cbr.ru%2Fscripts%2FXML_daily.asp%3Fdate_req%3D' + curDate + '%27&format=json&callback=', function(data) {
 		if (data != 'ERR') {
 			var rates = '';
-            $(data).find('ValCurs').each(function(){
-                if($(this).attr('Date')!=undefined) {
-                    var rate_data = $(this).attr('Date');
-                    var temp = $(this).attr('Date');
+            $(data.query.results.ValCurs).each(function(i, item){
+                    var rate_data = item.Date;
+                    var temp = rate_data;
                     var splArr = temp.split('/');
                     splArr[2] = splArr[2].substring(2,4);
                     rate_data = splArr.join('.');
                     $('#rate-data').html(rate_data);
                 }
-            });
+            );
 
-			$(data).find('Valute').each(function(key, value) {
-				var curCode = $(value).find('CharCode').html();
-				if (-1 != $.inArray(curCode, curCodes)) {
-					rates += '<li>' + $(value).find('Nominal').html()
-						+ ' ' + curCode
-						+ ' = ' + $(value).find('Value').html() + ' руб.' + '</li>';
-                    curRate = $(value).find('Value').html().substring(0,5);
+            $(data.query.results.ValCurs.Valute).each(function(key, value) {
+                if (-1 != $.inArray(value.CharCode, curCodes)) {
+                    curRate = value.Value.substring(0,5);
                     curRate = curRate.replace(/,/g, "\.");
                     curRate = parseFloat(curRate);
                     curRate = curRate.toString().substring(0,5);
-				}
-				$('#rates').html(rates);
+                }
+                $('#rates').html(rates);
                 $('#rate-cur').html(curRate);
                 $('#rate-rur').val(curRate);
-			});
+            });
 		}
 		else {
 			$('#rates').html('<li>Данные недоступны</li>');
